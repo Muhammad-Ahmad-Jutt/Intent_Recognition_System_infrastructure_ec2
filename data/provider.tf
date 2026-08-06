80a060dc14919c136e40ae7227932942cb13070c
Ahmad Arshad <ahmadarshad12345@gmail.com>
2026-08-06 13:29:29 +0100

    initial commit for simple ec2 instance

---
+++ data/provider.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+
+    tls = {
+      source = "hashicorp/tls"
+    }
+
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = "dummy"
+  secret_key = "dummy"
+  region     = "eu-west-1"
+
+  endpoints {
+    ec2 = "http://localstack-main:4566"
+    sts = "http://localstack-main:4566"
+    iam = "http://localstack-main:4566"
+  }
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+}
